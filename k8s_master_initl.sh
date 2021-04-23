89c479449fa28cd7a2a4c5837348e22f1d38acf1
aLessorSoul <huweinan.william@outlook.com>
2021-04-23 17:01:49 +0800

    k8s master init

---
+++ k8s_master_initl.sh
@@ -0,0 +1,5 @@
+kubeadm init --apiserver-advertise-address=192.168.101.130 --kubernetes-version v1.20.0 --image-repository registry.aliyuncs.com/google_containers --service-cidr=10.1.0.0/16 --pod-network-cidr=10.244.0.0/16 
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
