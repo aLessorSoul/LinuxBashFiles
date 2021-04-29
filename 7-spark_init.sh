3f337e0884ac4c1c02b5b3b29a89ddc63b457a0d
aLessorSoul <huweinan.william@outlook.com>
2021-04-30 01:36:31 +0800

    wow jupyterlab+pyspark on k8s running!

--- 7-spark_init.sh
+++ 7-spark_init.sh
@@ -3,7 +3,7 @@
 tar -xvf spark-2.4.7-bin-hadoop2.7.tgz
 mkdir -p /usr/local/
 sudo mv spark-2.4.7-bin-hadoop2.7 /usr/local/
-sudo ln -s /usr/local/spark-2.4.7-bin-hadoop2.7/ /usr/local/spark
+sudo ln -s /usr/local/spark /usr/local/spark-2.4.7-bin-hadoop2.7/ 
 
 cat <<EOF > /etc/profile 
 export PATH="/root/kubernetes/cluster/ubuntu/binaries/:$PATH"
